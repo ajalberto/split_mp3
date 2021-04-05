ac125338dd2e3fd1dfa586820beeed7ea1868048
venom <venom@venoms-MacBook-Pro.local>
2021-04-05 18:00:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import csv
+import subprocess
+import sys
+from pathlib import Path
+
+
+def main(f, tl):
+    target_path = ''
+    p = Path(f)
+    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+        target_path = str(p.parent)+'/splitted/'
+    elif sys.platform.startswith('win32'):
+        target_path = str(p.parent) + '\\splitted\\'
+
+    try:
+        os.mkdir(target_path)
+    except OSError:
+        print('Couldn\'t create directory (%s) for storing splitted files.' % target_path)
+    else:
+        csv.register_dialect('szr', delimiter=';', quoting=csv.QUOTE_NONE)
+        with open(tl, newline='') as csvfile:
+            reader = csv.reader(csvfile, 'szr')
+            next(reader)
+            for row in reader:
+                split_command = [
+                    'ffmpeg', '-i', f,
+                    '-acodec', 'copy', '-ss', row[1], '-to', row[2], target_path+row[0].strip(' ')+'.mp3'
+                ]
+                print(*split_command)
+                subprocess.run(split_command, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        print('Usage: %s file_to_split tracklist' % os.path.relpath(__file__))
